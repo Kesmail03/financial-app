8d71ac30efab9273ae78ff8a5a45d43687f3bde6
Khaled Esmail <kesmail@pdx.edu>
2025-01-11 23:46:25 -0800

    adding tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import "./index.css";
 
 function App() {
   const [data, setData] = useState([]);
