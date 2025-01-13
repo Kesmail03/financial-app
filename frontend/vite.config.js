09a88988ce8c1ef74d17f952667036ca88f347ee
Khaled Esmail <kesmail@pdx.edu>
2025-01-13 00:34:25 -0800

    running code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: "./postcss.config.js", // Point to your PostCSS configuration
+  server: {
+    proxy: {
+      "/api": "http://18.237.243.243:5000",
+      changeOrigin: true,
+      secure: false,
+    },
+    css: {
+      postcss: "./postcss.config.js", // Point to your PostCSS configuration
+    },
   },
 });
